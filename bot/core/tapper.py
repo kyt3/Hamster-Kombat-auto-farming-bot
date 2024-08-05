27bdea7e804effeca608c6a907f5d808186dc032
kyt3 <stalbutylka@gmail.com>
2024-08-05 16:10:09 +0200

    Remove some logs.

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -716,9 +716,6 @@ class Tapper:
 
                             queue = []
 
-                            max_level = 0
-                            cards_on_max_level = 0
-                            max_significance = 0
                             for upgrade in available_upgrades:
                                 upgrade_id = upgrade['id']
                                 level = upgrade['level']
@@ -726,17 +723,7 @@ class Tapper:
                                 current_profit = upgrade['currentProfitPerHour']
                                 profit = upgrade['profitPerHourDelta']
 
-                                if max_level < level:
-                                    max_level = level
-                                    cards_on_max_level = 0
-
-                                if max_level == level:
-                                    cards_on_max_level += 1
-
                                 significance = (profit + current_profit) / price if price > 0 else 0
-
-                                if max_significance < significance:
-                                    max_significance = significance
 
                                 if balance - price < settings.BALANCE_TO_SAVE:
                                     continue
@@ -747,10 +734,6 @@ class Tapper:
                                 if significance > settings.MIN_SIGNIFICANCE:
                                     queue.append([upgrade_id, significance, level, price, profit, current_profit,
                                                   upgrade['name']])
-
-                            logger.info(f"{self.session_name} | <lg>Max level of cards: {max_level} | "
-                                        f"Cards on max level: {cards_on_max_level} | "
-                                        f"Max significance: {max_significance} </lg>")
 
                             queue.sort(key=operator.itemgetter(1), reverse=True)
                             queue = queue[:10]
